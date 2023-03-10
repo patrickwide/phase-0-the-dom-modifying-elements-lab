da62109ccf84befe6eac18cf521a629bf1423b33
patrickwide <patrickwide254@gmail.com>
2023-03-10 20:43:08 +0300

    Completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,12 @@
-require ( './helpers.js' );
+require("./helpers.js");
 
 describe("index.html", () => {
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
-      expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+      expect(
+        document.querySelector("main#main"),
+        "Make sure you remove the <main> with id 'main'"
+      ).to.not.exist;
     });
 
     it("has a 'newHeader' variable that points to an <h1> node", () => {
@@ -21,8 +24,10 @@ describe("index.html", () => {
     });
 
     it("the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text \"YOUR-NAME is the champion\" (with your name in place of YOUR-NAME) inside it", () => {
-      expect(newHeader.innerHTML, "Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it").to.include("is the champion");
+      expect(
+        newHeader.innerHTML,
+        "Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it"
+      ).to.include("is the champion");
     });
-
   });
-})
+});
